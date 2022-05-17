ccd232e874f4443fc6adb640c28b4cd8b03b493d
Xavier Tintin <alextintin007@outlook.com>
2022-05-16 22:33:00 -0500

    My first commit

---
+++ src/agentes/Agente2.java
@@ -0,0 +1,75 @@
+package agentes;
+
+import contenidoSerializado.Sensores;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+import jdk.jshell.MethodSnippet;
+
+import java.util.logging.Level;
+
+public class Agente2 extends Agent {
+    @Override
+    protected void setup() {                //crea el primer hilo en la creacion del agente
+        addBehaviour(new Comportamiento());
+    }
+    class Comportamiento extends CyclicBehaviour {
+        int recEnv=0;
+        String cont[]= new String[2];
+        @Override
+        public void action(){
+            //todo lo que necesite hacer el agente
+            //ANN, AG, Bayes, if else
+            System.out.print("Nombre Agente 2: ");
+            System.out.println(getName());
+            ACLMessage msj = blockingReceive(); //this is blocking it till it recieves a message
+
+            String idC = msj.getConversationId();
+
+            if(idC.equalsIgnoreCase("COD0102")) {
+                try{
+                    System.out.println(msj);
+                    Sensores s = (Sensores) msj.getContentObject();
+                    System.out.println(s.getRiego()+" "+s.getTemperatura());
+                }catch (UnreadableException ex){
+                    Looger.getLogger(Agente2.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                /*String temperatura = msj.getContent();
+                if (Integer.parseInt(temperatura) > 35) {
+                    System.out.println("Prendiendo ventiladores");
+                    //...........
+                    Mensajes.enviar(ACLMessage.INFORM, "ReceptorInfo", "Ventilador prendido", "COD0201", getAgent());
+                    recEnv=1;
+                    cont[0]=temperatura;
+                }*/
+            }
+            else {
+                if(idC.equalsIgnoreCase("COD0302")) {
+                    String humedadHoja = msj.getContent();
+                    if (humedadHoja.equalsIgnoreCase("alta")) {
+                        System.out.println("No regar");
+                    }
+                    else{
+                        System.out.println("Regar");
+                    }
+
+                    if(recEnv==1){
+                        recEnv=2;
+                        cont[1]=humedadHoja;
+                    }
+                    Mensajes.enviar(ACLMessage.INFORM, "Ag3", "Estado de riesgo", "COD0203", getAgent());
+                }
+            }
+
+            if(recEnv==2){
+                Mensajes.enviar(ACLMessage.INFORM, "Ag4", "REVISAR PH", "COD0204", getAgent());
+                recEnv=0;
+            }
+            //System.out.println(msj);
+            //System.out.println(msj.getContent());
+            //System.out.println(msj.getConversationId());
+        }
+    }
+
+}
---
+++ src/agentes/Mensajes.java
@@ -0,0 +1,44 @@
+package agentes;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class Mensajes {
+    public static void enviar(int tipo, String recpetor, String contenidoMSJ, String codigoConversacionID, Agent emisor){
+        ACLMessage acl = new ACLMessage(tipo);
+
+        AID aid = new AID();                                //agent id
+        aid.setLocalName(recpetor);                         //pasamos al agente que envia
+
+        acl.addReceiver(aid);                               //agente que va a recibir
+        acl.setSender(emisor.getAID());                     //el que envia
+        acl.setLanguage(FIPANames.ContentLanguage.FIPA_SL);
+        acl.setContent(contenidoMSJ);
+        acl.setConversationId(codigoConversacionID);        //canal donde conversan los agentes
+
+        emisor.send(acl);                                   //agente que va a enviar
+    }
+    public static void enviarS(int tipo, String recpetor, Serializable object, String codigoConversacionID, Agent emisor) {
+        ACLMessage acl = new ACLMessage(tipo);
+
+        AID aid = new AID();
+        aid.setLocalName(recpetor);
+
+        acl.addReceiver(aid);
+        acl.setSender(emisor.getAID());
+        acl.setLanguage(FIPANames.ContentLanguage.FIPA_SL);
+        try {
+            acl.setContentObject(object);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        acl.setConversationId(codigoConversacionID);
+
+        emisor.send(acl);
+    }
+}
